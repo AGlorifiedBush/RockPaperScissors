346dfb2323a548f0302942ecce0e58ca55842a72
Matt Grych <james.grych@gmail.com>
2024-06-07 10:16:04 -0500

    Finishing Touches to the game
    Got everything working properly
    
    Won't accept entries other than "rock", "paper", or scissors

--- script.js
+++ script.js
@@ -2,65 +2,70 @@ console.log("Hello World")
 
 const choice = ["rock", "paper", "scissors"];
 
-function getComputerChoice(choice) {
+let humanScore = 0
+let computerScore = 0
+
+function getComputerChoice() {
     return(choice[Math.floor(Math.random()*choice.length)])
 }
 
 
-function getHumanChoice(choice) {
-    let playerChoice = prompt("what will you choose?", "Pssst... choose rock")
-    
-    return playerChoice.toLowerCase()
+function getHumanChoice() {
+    let playerChoice = prompt("what will you choose?", "Pssst... choose rock").toLowerCase()
+    while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+        let playerChoice = prompt("Try again! What will you choose?", "Pssst... choose rock")
+        return playerChoice
+    }
+    return playerChoice
 }
 
-function playGame() {
+function playRound(humanChoice, computerChoice){
+    console.log(computerChoice) 
 
-    function playRound(humanChoice, computerChoice){
-        console.log(computerChoice) 
-    
-        if (humanChoice === computerChoice) {
-            console.log("Its a Tie!")
-        }
-       
-        if (humanChoice === "rock") {
-            if (computerChoice === "scissors") {
-                console.log("Rock Beats Scissors! YOU WIN!")
-                humanScore++
-                return
-            }else if (computerChoice === "paper") {
-                console.log("Paper Beats Rock! YOU LOSE!")
-                computerScore++
-                return
-            } 
-        }
-    
-        if (humanChoice === "paper") {
-            if (computerChoice === "rock") {
-                console.log("Paper Beats Rock! YOU WIN!")
-                humanScore++
-                return
-            }else if (computerChoice === "scissors") {
-                console.log("Scissors Beats Paper! YOU LOSE!")
-                computerScore++
-                return
-            }
-        }
-    
-        if (humanChoice === "scissors") {
-            if (computerChoice === "rock") {
-                console.log("Rock Beats Scissors! YOU LOSE!")
-                computerScore++
-                return
-            }else if (computerChoice === "paper") {
-                console.log("Scissors Beats Paper! YOU WIN!")
-                humanScore++
-                return 
-            }
+    if (humanChoice === computerChoice) {
+        console.log("Its a Tie!")
+    }
+   
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            console.log("Rock Beats Scissors! YOU WIN!")
+            humanScore++
+            return
+        }else if (computerChoice === "paper") {
+            console.log("Paper Beats Rock! YOU LOSE!")
+            computerScore++
+            return
+        } 
+    }
+
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("Paper Beats Rock! YOU WIN!")
+            humanScore++
+            return
+        }else if (computerChoice === "scissors") {
+            console.log("Scissors Beats Paper! YOU LOSE!")
+            computerScore++
+            return
         }
     }
 
-    let humanScore = 0
-    let computerScore = 0
+    if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("Rock Beats Scissors! YOU LOSE!")
+            computerScore++
+            return
+        }else if (computerChoice === "paper") {
+            console.log("Scissors Beats Paper! YOU WIN!")
+            humanScore++
+            return 
+        }
+    }
+}
+
+
+
+function playGame() {
 
     for (let i = 0; i < 5; i++) {
 
@@ -74,4 +79,4 @@ function playGame() {
     }
 }
 
-console.log(playGame())
+playGame()
